b699e11ddebb9e13bdd360f8ede1afecd2fd8c50
sohaganorkar <sohaganorkar7720@gmail.com>
2021-06-10 15:35:54 +0530

    created preview cards for api data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import ActorGrid from '../components/actor/ActorGrid';
 import MainPageLayout from '../components/MainPageLayout';
+import ShowGrid from '../components/show/ShowGrid';
 import { apiGet } from '../misc/config';
 
 const Home = () => {
@@ -29,7 +31,7 @@ const Home = () => {
   const onRadioCange = ev => {
     setSearchOption(ev.target.value);
   };
-
+  // eslint-disable-next-line
   console.log(searchOption);
 
   const renderResults = () => {
@@ -38,11 +40,11 @@ const Home = () => {
     }
 
     if (results && results.length > 0) {
-      return results[0].show
-        ? results.map(item => <div key={item.show.id}>{item.show.name}</div>)
-        : results.map(item => (
-            <div key={item.person.id}>{item.person.name}</div>
-          ));
+      return results[0].show ? (
+        <ShowGrid data={results} />
+      ) : (
+        <ActorGrid data={results} />
+      );
     }
     return null;
   };
